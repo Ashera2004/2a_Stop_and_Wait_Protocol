6e05958e3468c9bf442b30db989fb16eceef94f1
Ashera2004 <79092592+Ashera2004@users.noreply.github.com>
2025-03-18 11:27:38 +0530

    Add files via upload

---
+++ Ex_2a_Client.py
@@ -0,0 +1,17 @@
+#Name: A S Siddarth
+#Reg No. 212224040316
+import socket 
+s=socket.socket() 
+s.bind(('localhost',8000))
+s.listen(5) 
+c,addr=s.accept() 
+while True: 
+    i=input("Enter a data: ") 
+    c.send(i.encode()) 
+    ack=c.recv(1024).decode() 
+    if ack: 
+        print(ack) 
+        continue 
+    else: 
+        c.close() 
+        break 
